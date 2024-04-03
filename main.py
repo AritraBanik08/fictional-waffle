735af815eb63c5b662351dcc9dc2ba352ea6610c
AritraBanik08 <banikaritra4@gmail.com>
2024-04-04 01:31:28 +0530

    012

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import database
 if __name__ == '__main__':
     cur = database.conn()
     database.create_table(cur)
-    print("Enter 1 to add task: \nEnter 2 to view tasks: \nEnter 3 to update task:")
-    a = int(input("Enter a number: "))
-    if a == 1:
-        task = input("Enter task: ")
-        database.add_task(cur, task)
-    elif a == 2:
-        tasks = database.view_tasks(cur)
-        for task in tasks:
-            print(task)
-    elif a == 3:
-        task_id = int(input("Enter task id: "))
-        database.update_task(cur, task_id)
-    else:
-        print("Invalid input")
+    print("Enter 1 to add task: \nEnter 2 to view tasks: \nEnter 3 to update task: \nPress any other key to exit:")
+    while True:
+        a = input("Enter a number: ")
+        if a == "1":
+            task = input("Enter task: ")
+            database.add_task(cur, task)
+        elif a == "2":
+            tasks = database.view_tasks(cur)
+            for task in tasks:
+                print(task)
+        elif a == "3":
+            task_id = int(input("Enter task id: "))
+            database.update_task(cur, task_id)
+        else:
+            break
